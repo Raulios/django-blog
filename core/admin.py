c4af59f0ac2cd2db04049e61a4ecb97fbfc03216
Raúl Molina <raulmolina88@gmail.com>
2016-02-12 10:26:37 +0100

    take out slug from exclude in admin page

--- core/admin.py
+++ core/admin.py
@@ -12,7 +12,6 @@ class PostAdmin(admin.ModelAdmin):
     '''
 
     list_display = ('id', 'title', 'created_at', 'updated_at')
-    exclude = ('slug',)
     list_display_links = ('id',)
     search_fields = ('title', 'categories', 'tags', 'created_at', 'updated_at')
 
